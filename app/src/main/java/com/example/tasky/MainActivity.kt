4f4745f558e3a601cf752542043b157d16cb1a57
ReptileSD <denbakushev@gmail.com>
2023-01-29 03:05:04 +1000

    added text editing and useless(yet) button

--- app/src/main/java/com/example/tasky/MainActivity.kt
+++ app/src/main/java/com/example/tasky/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.tasky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.tasky.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        binding.apply {
+            btnShow.setOnClickListener {
+                val title = etTitle.text
+                val note = etNote.text
+                tvNote.text = getString(R.string.note, title, note)
+            }
+        }
     }
 }
